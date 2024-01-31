32be87bfbaeb3119706ad2fecdac4fc6ef0aa493
Matheus Moura <matheusdossantosmoura64@gmail.com>
2024-01-31 13:43:26 -0300

    Atualizando arquivos

---
+++ C#/MeuApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MeuApp 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Testado";
+            Console.WriteLine(text);
+        }
+    }
+}
