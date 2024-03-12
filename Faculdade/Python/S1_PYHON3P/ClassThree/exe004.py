96acb257e7a0c8cd7b82f2d4d609772385bea803
Matheus Santos <matheusdossantosmoura64@gmail.com>
2024-03-12 19:59:30 -0300

    correção do arquivo exe004

--- Faculdade/Python/S1_PYHON3P/ClassThree/exe004.py
+++ Faculdade/Python/S1_PYHON3P/ClassThree/exe004.py
@@ -10,6 +10,8 @@ user = input('\nEscolha um opção: ')
 if(user.upper() == "A"):
     Fah = (9 * toConvert) / 5  + 32
     print(f'resultado: {Fah} graus Fahrenheit ')
-if(user == "B"):
+elif(user.upper() == "B"):
     Cel = ( 5 * (toConvert  - 32) ) / 9
     print(f'resultado {Cel} graus Celsius')
+else :
+    print('Opção invalida')
